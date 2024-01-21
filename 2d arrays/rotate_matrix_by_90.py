209a2ff4045efcd5400aac7d88cabd888e063979
NaveenGokavarapu19 <gokavarapu.naveen97@gmail.com>
2024-01-21 16:42:00 +0530

    placed the code at right path and added code for anti clockwise rotation

---
+++ 2d arrays/rotate_matrix_by_90.py
@@ -0,0 +1,48 @@
+n = int(input())
+matrix = []
+
+for _ in range(n):
+    rows = list(map(int,input().split()))
+    matrix.append(rows)
+    
+# tranpose first 
+
+for row in range(n):
+    for col in range(n):
+        if(row>col):
+            temp = matrix[row][col]
+            matrix[row][col] = matrix[col][row]
+            matrix[col][row] = temp
+
+# rotate the matrix by 90 clockwise
+
+print(matrix)
+
+
+# use the below code for clockwise 
+for k in range(n):
+    i = 0
+    j = n-1
+    while (i<j):
+        temp = matrix[k][i] 
+        matrix[k][i] = matrix[k][j]
+        matrix[k][j] = temp 
+        i = i + 1 
+        j = j -1
+
+
+#  uncomment the below code for anticlockwise
+        
+# for k in range(n):
+#     i = 0
+#     j = n-1
+#     while (i<j):
+#         temp = matrix[i][k] 
+#         matrix[i][k] = matrix[j][k]
+#         matrix[j][k] = temp 
+#         i = i + 1 
+#         j = j -1
+        
+print(matrix)
+     
+ 
